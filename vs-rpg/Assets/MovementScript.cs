3315e5fe9f7aeb57e1b6d689a2bfbc1ec9e943b1
Emil <emilbjorn101@gmail.com>
2026-06-26 07:54:16 +0200

    Added movement functionality
    Added movement functionality to player character using the default input action "Move".

--- vs-rpg/Assets/MovementScript.cs
+++ vs-rpg/Assets/MovementScript.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementScript : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 1f;
+    // Using default actions to move player character, see https://docs.unity3d.com/Packages/com.unity.inputsystem@1.19/manual/QuickStartGuide.html
+    InputAction moveAction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction = InputSystem.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveValue = moveAction.ReadValue<Vector2>();
+        Vector3 movement = new (moveValue.x, moveValue.y, 0f);
+        transform.position +=  moveSpeed * Time.deltaTime * movement;
     }
 }
