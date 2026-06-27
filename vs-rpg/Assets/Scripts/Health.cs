438c246f2aea238f0db306fdfbba03901dffb467
Emil <emilbjorn101@gmail.com>
2026-06-27 06:28:03 +0200

    Add health decreasing on collision
    Add on collision for an object with a health script, it will check if the player is getting hit by an enemy. If this is the case subtract 5 health from the player.

--- vs-rpg/Assets/Scripts/Health.cs
+++ vs-rpg/Assets/Scripts/Health.cs
@@ -14,4 +14,13 @@ public class Health : MonoBehaviour
     {
         
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (CompareTag("Player") & collision.gameObject.CompareTag("Enemy"))
+        {
+            health -= 5;
+            Debug.Log(health);
+        }
+    }
 }
